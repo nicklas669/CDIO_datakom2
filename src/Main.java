2178f4b82d8256569306c949173d77a47d691cf1
nicklas669 <nicklas669@gmail.com>
2015-04-06 23:00:14 +0200

    Commenced testing of FTP library.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SocketException, IOException {
 		// TODO Auto-generated method stub
-
+		FTPClient ftp;
+		ftp = new FTPClient();
+		ftp.connect("localhost");
+		System.out.println("Reply code: "+ftp.getReplyCode());
 	}
 
 }
