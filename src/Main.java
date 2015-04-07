481494fed435e1711454f3968b64bf5efb4cfd37
nicklas669 <nicklas669@gmail.com>
2015-04-07 12:52:56 +0200

    More FTP implemented.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 import org.apache.commons.net.ftp.*;
@@ -8,10 +8,16 @@ public class Main {
 
 	public static void main(String[] args) throws SocketException, IOException {
 		// TODO Auto-generated method stub
-		FTPClient ftp;
-		ftp = new FTPClient();
+		FTPClient ftp = new FTPClient();
 		ftp.connect("localhost");
+		ftp.login("root", "");
 		System.out.println("Reply code: "+ftp.getReplyCode());
+		FTPFile[] files = ftp.listFiles();
+		for (FTPFile file : files)	System.out.println(file.getName());
+		
+		FileOutputStream fos = new FileOutputStream("OSTEJOHN");
+		String remoteFilePath = "";
+		ftp.retrieveFile(remote, fos);
 	}
 
 }
